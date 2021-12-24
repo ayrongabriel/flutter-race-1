9d0950bf8d6f6d82fa223b6f239696581672e063
Ayron Gabriel <ayrongabriel@gmail.com>
2021-12-24 03:57:13 -0300

    Add métodos create, delete, update ao feed repository

--- lib/modules/home/pages/feed/repository/feed_repository.dart
+++ lib/modules/home/pages/feed/repository/feed_repository.dart
@@ -4,4 +4,15 @@ import 'package:meuapp/shared/models/product_model.dart';
 abstract class FeedRepository {
   Future<List<ProductModel>> all();
   Future<List<OrderModel>> allByUser({required String id_user});
+  Future<bool> create(
+      {required String id_user,
+      required String name,
+      required String price,
+      required String created_at});
+  Future<bool> delete({required String id});
+  Future<bool> update(
+      {required String id,
+      required String name,
+      required String price,
+      required String created_at});
 }
--- lib/modules/home/pages/feed/repository/feed_repository_impl.dart
+++ lib/modules/home/pages/feed/repository/feed_repository_impl.dart
@@ -22,4 +22,41 @@ class FeedRepositoryImpl implements FeedRepository {
         await database.allByUser(table: "orders", id_user: id_user);
     return (response).map((e) => OrderModel.fromMap(e)).toList();
   }
+
+  @override
+  Future<bool> create(
+      {required String id_user,
+      required String name,
+      required String price,
+      required String created_at}) async {
+    final priceParse = double.parse(price.replaceAll("R\$", ""));
+    final response = await database.create(table: "orders", data: {
+      "id_user": id_user,
+      "name": name.trim(),
+      "price": priceParse,
+      "created_at": created_at
+    });
+    return response;
+  }
+
+  @override
+  Future<bool> delete({required String id}) async {
+    final response = await database.delete(table: "orders", id: id);
+    return response;
+  }
+
+  @override
+  Future<bool> update(
+      {required String id,
+      required String name,
+      required String price,
+      required String created_at}) async {
+    final priceParse = double.parse(price.replaceAll("R\$", ""));
+    final response = await database.update(table: "orders", id: id, data: {
+      "name": name.trim(),
+      "price": priceParse,
+      "created_at": created_at
+    });
+    return response;
+  }
 }
