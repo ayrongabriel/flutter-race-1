9a7af71240454e204a564d063a9471b57b31e1a2
AyronGabriel <ayrongabriel@gmail.com>
2021-12-30 16:33:22 -0300

    Criação do controlador  profile

---
+++ lib/modules/profile/profileAvatar_controller.dart
@@ -0,0 +1,41 @@
+import 'dart:typed_data';
+
+import 'package:flutter/cupertino.dart';
+
+import 'package:meuapp/modules/profile/repository/profile_repository.dart';
+import 'package:meuapp/shared/utils/app_state.dart';
+
+class ProfileAvatarController extends ChangeNotifier {
+  AppState state = AppState.empty();
+  ProfileRepository repository;
+
+  ProfileAvatarController({
+    required this.repository,
+  });
+
+  void update(AppState state) {
+    this.state = state;
+    notifyListeners();
+  }
+
+  Future updateStorage({required String path, required Uint8List bytes}) async {
+    try {
+      update(AppState.loading());
+      final response = await repository.upload(filePath: path, bytes: bytes);
+      update(AppState.success<void>(response));
+      this.urlAvatar();
+    } catch (e) {
+      update(AppState.error(e.toString()));
+    }
+  }
+
+  Future<void> urlAvatar() async {
+    try {
+      update(AppState.loading());
+      final response = await repository.avatarUrl();
+      update(AppState.success<String?>(response));
+    } catch (e) {
+      update(AppState.error(e.toString()));
+    }
+  }
+}
--- lib/modules/profile/profile_controller.dart
+++ lib/modules/profile/profile_controller.dart
@@ -1,47 +1,61 @@
-import 'dart:io';
-
 import 'package:flutter/cupertino.dart';
+
 import 'package:meuapp/modules/profile/repository/profile_repository.dart';
+import 'package:meuapp/shared/models/user_model.dart';
 import 'package:meuapp/shared/utils/app_state.dart';
 
 class ProfileController extends ChangeNotifier {
   AppState state = AppState.empty();
   ProfileRepository repository;
+  UserModel userModel;
+
+  final formKey = GlobalKey<FormState>();
+  String _name = "";
 
   ProfileController({
     required this.repository,
+    required this.userModel,
   });
+
+  void onChange({String? name}) {
+    _name = name ?? _name;
+    print(name);
+  }
+
+  bool validate() {
+    final form = formKey.currentState!;
+    if (form.validate()) {
+      form.save();
+      return true;
+    }
+    return false;
+  }
 
   void update(AppState state) {
     this.state = state;
     notifyListeners();
   }
 
-  Future updateStorage({required String path, required File file}) async {
-    try {
-      update(AppState.loading());
-      final response = await repository.upload(path: path, file: file);
-      update(AppState.success<String>(response));
-    } catch (e) {
-      update(AppState.error(e.toString()));
-    }
-  }
-
-  String urlAvatar({required String? path}) {
-    // if (path == null) return;
-    var response;
-    () async {
+  Future<void> updateProfile() async {
+    if (validate()) {
       try {
         update(AppState.loading());
-        response = await repository.getPublicUrl(path: path!);
-        print("teste response: ${response}");
-        update(AppState.success<String>(response));
+        final response = await repository.update(id: userModel.id, name: _name);
+        this.getProfile();
+        update(AppState.success<UserModel>(response));
       } catch (e) {
         update(AppState.error(e.toString()));
       }
-    };
-    if (response == null) return "";
+    }
+  }
 
-    return response;
+  Future<void> getProfile() async {
+    try {
+      update(AppState.loading());
+      final response = await repository.getProfile(id: userModel.id);
+      update(AppState.success<UserModel>(response));
+    } catch (e) {
+      update(AppState.error(e.toString()));
+    }
   }
 }
