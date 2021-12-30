faa796a97c580ffe3cace7ae28f85cdb70f9fe62
AyronGabriel <ayrongabriel@gmail.com>
2021-12-30 16:34:49 -0300

    Add opção de descrição do produto e inserir imagem

--- lib/modules/product/product_bottomsheet.dart
+++ lib/modules/product/product_bottomsheet.dart
@@ -1,5 +1,9 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_multi_formatter/flutter_multi_formatter.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:intl/intl.dart';
 
 import 'package:meuapp/modules/product/product_controller.dart';
@@ -9,6 +13,7 @@ import 'package:meuapp/shared/services/app_database.dart';
 import 'package:meuapp/shared/theme/app_theme.dart';
 import 'package:meuapp/shared/widgets/button/button_widget.dart';
 import 'package:meuapp/shared/widgets/input_text/input_text.dart';
+import 'package:meuapp/shared/widgets/modal/app_modal_image_picker.dart';
 
 class ProductBottomSheet extends StatefulWidget {
   final UserModel user;
@@ -24,6 +29,10 @@ class ProductBottomSheet extends StatefulWidget {
 class _ProductBottomSheetState extends State<ProductBottomSheet> {
   late final ProductController controller;
   var _selectedDate;
+  var _bytesImage;
+  var _filePath;
+  var _imgTemp;
+  bool _isLoading = false;
 
   @override
   void initState() {
@@ -45,6 +54,40 @@ class _ProductBottomSheetState extends State<ProductBottomSheet> {
   void dispose() {
     controller.dispose();
     super.dispose();
+  }
+
+  Future imagePicker({required ImageSource source, int? quality}) async {
+    try {
+      final imageFile =
+          await ImagePicker().pickImage(source: source, imageQuality: quality);
+      if (imageFile == null) return;
+
+      setState(() => _isLoading = true);
+
+      _bytesImage = await imageFile.readAsBytes();
+      final fileExt = imageFile.name.split('.').last;
+      final fileName = '${DateTime.now().toIso8601String()}.$fileExt';
+      _filePath = fileName;
+
+      setState(() {
+        _imgTemp = File(imageFile.path);
+      });
+
+      setState(() => false);
+    } on PlatformException catch (e) {
+      print(e.message);
+    }
+  }
+
+  Future _showModalImagePicker() async {
+    await showModalBottomSheet(
+        context: context,
+        builder: (context) {
+          return AppModalImagePicker(
+            souceCamera: () => imagePicker(source: ImageSource.camera),
+            souceGalery: () => imagePicker(source: ImageSource.gallery),
+          );
+        });
   }
 
   _showDatePiker() async {
@@ -80,15 +123,15 @@ class _ProductBottomSheetState extends State<ProductBottomSheet> {
             children: [
               SizedBox(height: 32),
               InputText(
-                label: "Produto",
-                hint: "Digite um nome",
+                label: "Produto *",
+                hint: "Digite o nome",
                 onChanged: (value) => controller.onChange(name: value),
                 validator: (value) =>
                     value.isNotEmpty ? null : "Favor digite o nome do produto",
               ),
               SizedBox(height: 8),
               InputText(
-                label: "Preço",
+                label: "Preço *",
                 hint: "Digite o valor",
                 keyboardType: TextInputType.number,
                 inputFormatters: [
@@ -103,11 +146,18 @@ class _ProductBottomSheetState extends State<ProductBottomSheet> {
                     value.isNotEmpty ? null : "Favor digite o valor do produto",
               ),
               SizedBox(height: 8),
+              InputText(
+                label: "Descrição",
+                hint: "Descrição da compra...",
+                maxLines: 5,
+                onChanged: (value) => controller.onChange(description: value),
+              ),
+              SizedBox(height: 8),
               Row(
                 children: [
                   Expanded(
                     child: Text(_selectedDate == null
-                        ? 'Data da compra'
+                        ? 'Data da compra *'
                         : "Data selecionada: ${DateFormat('dd/MM/y').format(_selectedDate)}"),
                   ),
                   TextButton(
@@ -116,6 +166,43 @@ class _ProductBottomSheetState extends State<ProductBottomSheet> {
                   ),
                 ],
               ),
+              SizedBox(height: 8),
+              _isLoading
+                  ? GestureDetector(
+                      onTap: () {
+                        setState(() {
+                          _isLoading = false;
+                        });
+                      },
+                      child: Container(
+                        height: 150,
+                        width: 150,
+                        decoration: BoxDecoration(
+                          image: DecorationImage(
+                              image: FileImage(_imgTemp), fit: BoxFit.cover),
+                        ),
+                        child: Center(
+                          child: ClipOval(
+                            child: Container(
+                              color:
+                                  AppTheme.colors.background.withOpacity(0.6),
+                              height: 50,
+                              width: 50,
+                              child: Center(
+                                child: Icon(
+                                  Icons.delete,
+                                  color: AppTheme.colors.badColor,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    )
+                  : ElevatedButton(
+                      onPressed: _showModalImagePicker,
+                      child: Icon(Icons.camera_alt_outlined),
+                    ),
               SizedBox(height: 28),
               AnimatedBuilder(
                 animation: controller,
@@ -144,8 +231,8 @@ class _ProductBottomSheetState extends State<ProductBottomSheet> {
                       Button(
                         label: "Adicionar",
                         onTap: () {
-                          controller.create();
-                          // print("Clicou em cadastrar produto");
+                          controller.create(
+                              path: _filePath, bytesImage: _bytesImage);
                         },
                       )
                     ],
@@ -153,8 +240,8 @@ class _ProductBottomSheetState extends State<ProductBottomSheet> {
                   orElse: () => Button(
                     label: "Adicionar",
                     onTap: () {
-                      controller.create();
-                      // print("Clicou em cadastrar produto");
+                      controller.create(
+                          path: _filePath, bytesImage: _bytesImage);
                     },
                   ),
                 ),
