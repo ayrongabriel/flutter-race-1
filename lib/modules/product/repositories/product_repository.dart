3d8674154b263df5a716faa9ef5d5501aeb570e7
AyronGabriel <ayrongabriel@gmail.com>
2021-12-30 18:08:22 -0300

    add novos parametros

--- lib/modules/product/repositories/product_repository.dart
+++ lib/modules/product/repositories/product_repository.dart
@@ -1,12 +1,19 @@
+import 'dart:typed_data';
+
 abstract class ProductRepository {
   Future<bool> create(
       {required String id_user,
       required String name,
       required String price,
-      required String created_at});
+      String? description,
+      required String created_at,
+      String? path,
+      Uint8List? bytesImage});
   Future<bool> update(
       {required String id,
       required String name,
       required String price,
-      required String created_at});
+      String? description});
+  Future<void> upload({required String filePath, required Uint8List bytes});
+  Future<String?> avatarUrl();
 }
--- lib/modules/product/repositories/product_repository_impl.dart
+++ lib/modules/product/repositories/product_repository_impl.dart
@@ -1,3 +1,5 @@
+import 'dart:typed_data';
+
 import 'package:meuapp/modules/product/repositories/product_repository.dart';
 import 'package:meuapp/shared/services/app_database.dart';
 
@@ -12,29 +14,57 @@ class ProductRepositoryImpl implements ProductRepository {
       {required String id_user,
       required String name,
       required String price,
-      required String created_at}) async {
+      String? description,
+      required String created_at,
+      String? path,
+      Uint8List? bytesImage}) async {
     final priceParse = double.parse(price.replaceAll("R\$", ""));
     final response = await database.create(table: "orders", data: {
       "id_user": id_user,
       "name": name.trim(),
       "price": priceParse,
+      "description": description ?? "",
       "created_at": created_at
+    });
+    if (path != null && bytesImage != null) {
+      upload(filePath: path, bytes: bytesImage);
+    }
+    return response;
+  }
+
+  @override
+  Future<bool> update({
+    required String id,
+    required String name,
+    required String price,
+    String? description,
+  }) async {
+    final priceParse = double.parse(price.replaceAll("R\$", ""));
+    final response = await database.update(table: "orders", id: id, data: {
+      "name": name.trim(),
+      "price": priceParse,
+      "description": description ?? "",
+      "updated_at": DateTime.now().toIso8601String()
     });
     return response;
   }
 
   @override
-  Future<bool> update(
-      {required String id,
-      required String name,
-      required String price,
-      required String created_at}) async {
-    final priceParse = double.parse(price.replaceAll("R\$", ""));
-    final response = await database.update(table: "orders", id: id, data: {
-      "name": name.trim(),
-      "price": priceParse,
-      "created_at": created_at
-    });
+  Future<void> upload(
+      {required String filePath, required Uint8List bytes}) async {
+    final response = await database.uploadStorage(
+        bucket: 'products',
+        path: filePath,
+        bytes: bytes,
+        table: 'orders',
+        column: 'thumbnail_url');
     return response;
+  }
+
+  @override
+  Future<String?> avatarUrl() async {
+    final response = await database.getPublicUrl(
+        table: "orders", column: "tumbnail_url", bucket: "products");
+    return response!;
   }
 }
